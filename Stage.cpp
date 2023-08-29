b8fec26d1d39a738962ce3bc1f86badcab45dcda
頼慶 八窪 <220630@jc-21.jp>
2023-08-29 16:35:21 +0900

    ステージ調整中

--- Stage.cpp
+++ Stage.cpp
@@ -1,9 +1,12 @@
+#include <string>
+
 #include "Engine/Model.h"
+
 #include "Stage.h"
 
 //コンストラクタ
 Stage::Stage(GameObject* parent)
-    :GameObject(parent, "Stage"), hModel_(-1), width_(15), height_(15)
+    :GameObject(parent, "Stage"), hModel_{0,0,0,0,0}, width_(15), height_(15)
 {
 }
 
@@ -15,9 +18,22 @@ Stage::~Stage()
 //初期化
 void Stage::Initialize()
 {
+    std::string failBase = "Assets/";
+    std::string modelName[] =
+    {
+        "BoxDefault.fbx",
+        "BoxBrick.fbx",
+        "BoxGrass.fbx",
+        "BoxSand.fbx",
+        "BoxWater.fbx"
+    };
+    
     //モデルデータのロード
-    hModel_ = Model::Load("Assets/BoxDefault.fbx");
-    assert(hModel_ >= 0);
+    for (int i = 0; i < MODEL_NUM; i++)
+    {
+        hModel_[i] = Model::Load(failBase + modelName[i]);
+        assert(hModel_ >= 0);
+    }
 }
 
 //更新
@@ -47,3 +63,103 @@ void Stage::Draw()
 void Stage::Release()
 {
 }
+
+//pacman
+#if 0
+#include "Stage.h"
+
+#include "Engine/Model.h"
+#include "Engine/CsvReader.h"
+
+//コンストラクタ
+Stage::Stage(GameObject* parent)
+    :GameObject(parent, "Stage"), hModel_{ -1,-1 }, table_(nullptr)
+{
+    CsvReader csv;
+    csv.Load("map_01.csv");
+
+    width_ = csv.GetWidth();    //１行に何個データがあるか
+    height_ = csv.GetHeight();   //データが何行あるか
+
+    table_ = new int* [width_];
+
+    for (int x = 0; x < width_; x++)
+    {
+        table_[x] = new int[height_];
+        for (int z = 0; z < height_; z++)
+        {
+            table_[x][height_ - 1 - z] = csv.GetValue(x, z);
+        }
+    }
+}
+
+//デストラクタ
+Stage::~Stage()
+{
+}
+
+//初期化
+void Stage::Initialize()
+{
+    const char* fileName[] =
+    {
+        "Floor.fbx",
+        "Wall.fbx"
+    };
+
+    //モデルデータのロード
+    for (int i = 0; i < TYPE_MAX; i++)
+    {
+        hModel_[i] = Model::Load(fileName[i]);
+        assert(hModel_[i] >= 0);
+    }
+
+
+}
+
+//更新
+void Stage::Update()
+{
+}
+
+//描画
+void Stage::Draw()
+{
+    Transform blockTrans;
+
+    for (int x = 0; x < width_; x++)
+    {
+        for (int z = 0; z < height_; z++)
+        {
+            blockTrans.position_.x = x;
+            blockTrans.position_.z = z;
+
+            int type = table_[x][z];
+
+            Model::SetTransform(hModel_[type], blockTrans);
+            Model::Draw(hModel_[type]);
+        }
+    }
+
+}
+
+//開放
+void Stage::Release()
+{
+    for (int x = 0; x < width_; x++)
+    {
+        delete[] table_[x];
+    }
+    delete[] table_;
+}
+
+//壁床判定
+bool Stage::IsWall(int x, int z)
+{
+    if (table_[x][z] == 0)
+    {
+        return true;
+    }
+    return false;
+}
+#endif
--- Stage.h
+++ Stage.h
@@ -1,10 +1,59 @@
 #pragma once
 #include "Engine/GameObject.h"
 
+enum
+{
+    TYPE_DEFAULT=0,
+    TYPE_BRICK,
+    TYPE_GRASS,
+    TYPE_SAND,
+    TYPE_WATER,
+    TYPE_MAX
+};
+
 //ステージを管理するクラス
 class Stage : public GameObject
 {
-    int hModel_;
+    int hModel_[TYPE_MAX];      //モデル
+    int sizeY_, sizeX_, sizeZ_; //ステージサイズ
+    int** table_;               //地面
+public:
+    //コンストラクタ
+    Stage(GameObject* parent);
+
+    //デストラクタ
+    ~Stage();
+
+    //初期化
+    void Initialize() override;
+
+    //更新
+    void Update() override;
+
+    //描画
+    void Draw() override;
+
+    //開放
+    void Release() override;
+};
+
+//pacman
+#if 0
+#pragma once
+#include "Engine/GameObject.h"
+
+enum
+{
+    TYPE_FLOOR,
+    TYPE_WALL,
+    TYPE_MAX,
+};
+
+//地面を管理するクラス
+class Stage : public GameObject
+{
+    int hModel_[TYPE_MAX];
+    int** table_;
     int height_;
     int width_;
 public:
@@ -25,4 +74,11 @@ public:
 
     //開放
     void Release() override;
+
+    //床と壁を判定するためのゲッター(通れるor通れない)
+    //引数     ：ｘ,ｚ        調べる位置
+    //戻り値   ：             座標
+    bool IsWall(int x, int z);
+
 };
+#endif
